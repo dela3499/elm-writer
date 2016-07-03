bdb3a5b70d14dfbf5d593add6a171ce639eb2479
Carlos <carlos@bioagelabs.com>
2016-07-03 19:02:27 -0400

    Removed unused imports.

--- main.elm
+++ main.elm
@@ -1,26 +1,13 @@
 port module WordCounter exposing ( .. )
 
-import Html exposing ( Html, text, div, span, input, textarea, img, table, tr, th, td, i, p, a )
-import Html.Events exposing ( on, targetValue, onClick, onDoubleClick, onInput )
+import Html exposing ( Html, text, div, textarea )
+import Html.Events exposing ( onInput )
 import Html.Attributes exposing ( .. )
 import Html.App as Html
-import Color exposing ( .. )
 
-import Maybe exposing ( withDefault, andThen )
-
-import CollectionsNg.Array as Array exposing ( Array )
-import Set exposing ( Set )
-import Dict exposing ( Dict )
-import Random
-import Result
 import List
-import Regex
 import String
 import Time exposing ( .. )
-import Window
-import Mouse
-import Task
-import Debug
 
 
 type Msg 
